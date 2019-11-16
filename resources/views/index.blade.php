75b3c53c2228c54eb6e67031fda5da96b049e9ae
Alexander Bachschmid <alexander.bachschmid@prochannel.eu>
2019-11-16 05:05:50 +0100

    Start with papercss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://unpkg.com/papercss@1.6.1/dist/paper.min.css">
+  <title>Anonyme Ideen</title>
+</head>
+<body>
+  <div class="paper container">
+    <h1>Some Fresh Title</h1>
+    <div class="form-group">
+        <label for="paperInputs1">Input</label>
+        <input type="text" placeholder="Nice input" id="paperInputs1">
+      </div>
+  </div>
+</body>
+</html>
